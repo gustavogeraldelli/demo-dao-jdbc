7cd1a47b2bf678e458f868e1f24c4ecd7d9d7530
gustavogeraldelli <geraldellig@gmail.com>
2023-09-14 20:11:45 -0300

    Created DaoFactory and adjusting DAOs

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -6,8 +6,8 @@ import model.entities.Department;
 
 public interface DepartmentDao {
 
-	void insert(Department obj);
-	void update(Department obj);
+	void insert(Department d);
+	void update(Department d);
 	void deleteById(Integer id);
 	Department findById(Integer id);
 	List<Department> findAll();
